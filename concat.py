8be13e0d4c0317d343a364cf5a8e7e5f9c96deb5
shahsadm99 <shahsadm99@gmail.com>
2018-03-08 15:50:47 +0530

    accepted keys from keyboard and concatenated them

--- concat.py
+++ concat.py
@@ -6,11 +6,10 @@ import cv2
 
 
 
+
+
 def autocrop(image, threshold=0):
-    """Crops any edges below or equal to threshold
-    Crops blank image to 1x1.
-    Returns cropped image.
-    """
+
     if len(image.shape) == 3:
         flatImage = np.max(image, 2)
     else:
@@ -25,11 +24,33 @@ def autocrop(image, threshold=0):
         image = image[:1, :1]
 
     return image
+data=pd.read_csv("train.csv")
+#data6=pd.read_csv("train.csv").as_matrix()
+print (data)
 
-data=pd.read_csv("train.csv").as_matrix()
 
-data_1=data[0:1,1:]
-data_2=data[1:2,1:]
+print("enter a no")
+input_x=int(input())
+#df2 = pd.DataFrame([[input_x]], columns=list('A'))
+#print(df2)
+#a=df2.iat[0,0]
+#print(df2.iat[0,0])
+print(input_x)
+print("enter a no")
+input_y=int(input())
+print(input_y)
+#result_1=pd.filter(like=input_x, axis=0)
+#print (result_1)
+result_1 = data[data["label"]==input_x].as_matrix()
+print(result_1)
+result_2 = data[data['label']==input_y].as_matrix()
+data_1=result_1[0:1,1:]
+data_2=result_2[0:1,1:]
+#data_2=data[1:2,1:]
+
+
+
+
 #pt.imshow(data_1)
 data_1.shape=(28,28)
 pt.imshow(255-data_1,cmap='gray')
