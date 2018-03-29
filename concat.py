4d7d527ff1feab238ef9873bbf71ae9592b12a9c
shahsadm99 <shahsadm99@gmail.com>
2018-03-29 18:44:44 +0530

    concatinating multiple characters

--- concat.py
+++ concat.py
@@ -28,7 +28,10 @@ data=pd.read_csv("train.csv")
 #data6=pd.read_csv("train.csv").as_matrix()
 print (data)
 
-
+print("enter a string")
+input_string=input()
+input_list=list(input_string)
+print(input_list)
 print("enter a no")
 input_x=int(input())
 #df2 = pd.DataFrame([[input_x]], columns=list('A'))
@@ -41,30 +44,38 @@ input_y=int(input())
 print(input_y)
 #result_1=pd.filter(like=input_x, axis=0)
 #print (result_1)
-result_1 = data[data["label"]==input_x].as_matrix()
-print(result_1)
-result_2 = data[data['label']==input_y].as_matrix()
-data_1=result_1[0:1,1:]
-data_2=result_2[0:1,1:]
-#data_2=data[1:2,1:]
+
+def conc(input_x,input_y):
+    result_1 = data[data["label"]==input_x].as_matrix()
+    print(result_1)
+    result_2 = data[data['label']==input_y].as_matrix()
+    data_1=result_1[0:1,1:]
+    data_2=result_2[0:1,1:]
+    #data_2=data[1:2,1:]
 
 
 
 
-#pt.imshow(data_1)
-data_1.shape=(28,28)
-pt.imshow(255-data_1,cmap='gray')
-pt.show()
-data_3=autocrop(data_1)
-pt.imshow(255-data_3,cmap='gray')
-pt.show()
-data_2.shape=(28,28)
-pt.imshow(255-data_2,cmap='gray')
-pt.show()
-data_4=autocrop(data_2)
-pt.imshow(255-data_4,cmap='gray')
-pt.show()
-vis = np.concatenate((data_3, data_4), axis=1)
-#vis = np.concatenate((data_1, data_2), axis=0)
-pt.imshow(255-vis,cmap='gray')
-pt.show()
+    #pt.imshow(data_1)
+    data_1.shape=(28,28)
+    pt.imshow(255-data_1,cmap='gray')
+    pt.show()
+    data_3=autocrop(data_1)
+    pt.imshow(255-data_3,cmap='gray')
+    pt.show()
+    data_2.shape=(28,28)
+    pt.imshow(255-data_2,cmap='gray')
+    pt.show()
+    data_4=autocrop(data_2)
+    pt.imshow(255-data_4,cmap='gray')
+    pt.show()
+    vis = np.concatenate((data_3, data_4), axis=1)
+    #vis = np.concatenate((data_1, data_2), axis=0)
+    pt.imshow(255-vis,cmap='gray')
+    pt.show()
+length_list=len(input_list)
+print(length_list)
+for i in range(0,length_list-1):
+    input_x=int(input_list[i])
+    input_y=int(input_list[i+1])
+    conc(input_x,input_y)
